75c2a3bf5566d129b91d0811bb26e5fc25a11b84
pizzi9523 <pizzitolagiuseppe@live.it>
2022-01-27 16:58:45 +0100

    Create Relation between Category and Post

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'image', 'body'];
+    protected $fillable = ['title', 'image', 'body', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
