51104d8f3dc81bee4a344a81eb56715f43b2cf83
pizzi9523 <pizzitolagiuseppe@live.it>
2022-02-02 15:14:20 +0100

    fix image in view tag/post

--- resources/views/guest/tags/posts.blade.php
+++ resources/views/guest/tags/posts.blade.php
@@ -10,7 +10,8 @@
                 <div class="col-4 mb-4">
                     <a class="text-decoration-none text-dark" href="{{ route('posts.show', $post->id) }}">
                         <div class="card" style="width: 18rem;">
-                            <img height="200" src="{{ $post->image }}" class="card-img-top" alt="...">
+                            <img height="200" src="{{ asset('storage/' . $post->image) }}" class="card-img-top"
+                                alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $post->title }}</h5>
                                 <p class="card-text">{{ $post->body }}</p>
