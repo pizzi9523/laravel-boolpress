c14099c06418046d20d7551fe13ee300d341e899
pizzi9523 <pizzitolagiuseppe@live.it>
2022-02-02 21:46:11 +0100

    add tags to post card

--- resources/views/guest/posts/blog.blade.php
+++ resources/views/guest/posts/blog.blade.php
@@ -3,25 +3,27 @@
 @section('content')
 
     <h1 class="text-center">SPA Blog</h1>
-    <div class="row flex-wrap">
 
-        <div class="container">
-            <div class="row">
-                <div class="col-4 my-2" v-for="post in posts">
-                    <div class="card">
-                        <img class="card-img-top" :src="'storage/' + post.image" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">@{{ post.title }}</h4>
-                            <p class="card-text">@{{ post.body }}</p>
-                            <p class="card-text">@{{ post.category.name }}</p>
+    <div class="container">
+        <div class="row">
 
-                        </div>
+            <div class="col-4 my-2" v-for="post in posts">
+                <div class="card">
+                    <img class="card-img-top" :src="'storage/' + post.image" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">@{{ post.title }}</h4>
+                        <p class="card-text">@{{ post.body }}</p>
+                        <p class="card-text">@{{ post.category.name }}</p>
+                        <p class="card-text">
+                            <span v-for="tag in post.tags">- @{{ tag.name }} </span>
+                        </p>
+
                     </div>
                 </div>
-
             </div>
-        </div>
 
+        </div>
     </div>
+
 
 @endsection
