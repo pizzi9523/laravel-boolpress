87bd93e65c90a62b4f46ec67bd52c93989535f6b
pizzi9523 <pizzitolagiuseppe@live.it>
2022-02-04 11:07:28 +0100

    delete pagination component

--- resources/js/components/PostListComponent.vue
+++ resources/js/components/PostListComponent.vue
@@ -15,7 +15,21 @@
           </div>
         </div>
       </div>
-      <pagination-component></pagination-component>
+      <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
 
     <!-- <div class="pagination">
@@ -27,11 +41,7 @@
 </template>
 
 <script>
-import PaginationComponent from "../components/PaginationComponent.vue";
-
 export default {
-  components: { PaginationComponent },
-
   data() {
     return {
       posts: null,
